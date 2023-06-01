8d3b23239d798fbe2ebeee9ffdd6744ed2be15a2
ElizzeT <84882564+ElizzeT@users.noreply.github.com>
2023-06-01 09:11:57 -0600

    Create class.py

---
+++ class.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from keras.utils import to_categorical
+
+new_data = pd.read_csv('nuevo_audio_mfcc.csv')
+
+model = tf.keras.models.load_model('rednc.h5')
+
+predictions = model.predict(new_data)
+predicted_classes = np.argmax(predictions, axis=1)
+
+
+for prediction in predicted_classes:
+    if prediction == 1:
+        print("Clase predicha: hambre")
+    elif prediction == 2:
+        print("Clase predicha: descontento")
+    else:
+        print("Clase predicha desconocida")
+        
+print("Predicciones:", predicted_classes)
+
+label_names = ['1', '2']  # Reemplaza con los nombres de tus clases
+
+predicted_labels = [label_names[prediction] for prediction in predicted_classes]
+print("Clases predichas:", predicted_labels)
+
